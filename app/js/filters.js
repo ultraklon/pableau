41de5e9bfcc811db926080b986780aa889d1970a
Pablo Moyano <ultraklon@gmail.com>
2014-03-28 23:45:24 -0700

    see last commit's comment

--- app/js/filters.js
+++ app/js/filters.js
@@ -7,4 +7,27 @@ angular.module('myApp.filters', []).
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }]).
+  filter('onlyNonNumericFilter', function() {
+    return function(array) {
+      var retVal = new Array();
+      for(var i in array){
+        if(array[i].type == "string"){
+          retVal.push(array[i]);
+        }
+      }
+      return retVal;
+    }
+  }).
+  filter('onlyNumericFilter', function() {
+    return function(array) {
+      var retVal = new Array();
+      for(var i in array){
+        if(array[i].type == "numeric"){
+          retVal.push(array[i]);
+        }
+      }
+      return retVal;
+    }
+  })
+;
